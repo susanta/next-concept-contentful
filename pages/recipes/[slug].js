b894071fa1355e964dfdf0b52040420ee3fc1a11
Susanta <inbox.susanta@gmail.com>
2022-02-04 20:51:34 +0530

    Incremental Static Regeneration

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -33,11 +33,12 @@ export async function getStaticProps({ params }) {
 
   return {
     props: { recipe: items[0] },
+    revalidate: 1,
   };
 }
 
 export default function RecipeDetails({ recipe }) {
-  console.log(recipe);
+  // console.log(recipe);
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
   return (
